1c289530f943d4e71d8d2b2a9ed4a95e57c70f21
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 21:04:14 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
 
 
+def my_collect(array)
+      i = 0
+      newarray = []
+      while i < array.length
+            newarray << yield(array[i])
+            i += 1
+      end
+      newarray
+end
+
+languages =  ['ruby', 'javascript', 'python', 'objective-c']
+students =  ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
+puts my_collect(languages) {|name| name.upcase}
+puts my_collect(students) {|name| name.split(" ").first}
