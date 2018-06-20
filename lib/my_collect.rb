79a711b027e9f4278806479fcda4cd2ecd435879
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 20:57:58 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
 
 
+def my_collect(array)
+      i = 0
+      while i < array.length
+            yield(array[i])
+            i += 1
+      end
+      array
+end
+
+my_collect(array) {|name| name.upcase}
