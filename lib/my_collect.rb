5627319e6d01f0a5295aafe3ed7ced857a628a3e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-20 20:59:24 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,7 @@ def my_collect(array)
       i = 0
       newarray = []
       while i < array.length
-            newarra << yield(array[i])
+            newarray << yield(array[i])
             i += 1
       end
       newarray
